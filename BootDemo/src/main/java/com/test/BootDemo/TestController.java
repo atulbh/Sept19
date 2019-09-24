a620e7a1be6bc470d1cce1063e65b9137df3167f
atul.kumar <atul.krbhatia@gmail.com>
2019-09-24 13:29:15 +0530

    commit

---
+++ BootDemo/src/main/java/com/test/BootDemo/TestController.java
@@ -0,0 +1,19 @@
+package com.test.BootDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@GetMapping("/")
+	public String sayHi(){
+		return "hello thr!";
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "hello thr! this is new";
+	}
+}
