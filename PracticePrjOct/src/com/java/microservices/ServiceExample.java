5791b49b710b0063758645f7f72e60ecc8f62728
Atul <atul.krbhatia@gmail.com>
2019-10-11 17:23:10 +0530

    added JPA steps

--- PracticePrjOct/src/com/java/microservices/ServiceExample.java
+++ PracticePrjOct/src/com/java/microservices/ServiceExample.java
@@ -1,5 +1,9 @@
 package com.java.microservices;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.in28minutes.microservices.currencyexchangeservice.ExchangeValue;
+
 public class ServiceExample {
 
 	// Step1. when we create a microservice we use @SpringBootApplication and if we need to read values from
@@ -16,5 +20,15 @@ public class ServiceExample {
 	//spring.application.name=limits-service
 	//spring.cloud.config.uri=http://localhost:8888
 	//spring.profiles.active=dev
+	
+	//Step4. Adding JPA, to add JPA, it must have included Spring JPA and H2 db dependency.
+	//Convert your bean into Entity
+	//create a data.sql file to insert some records, just write some insert queries and put file in 
+	//resources, when spring boots up that will be executed and records will be insterted in H2 DB
+	//that we can check in localhost:port/he-console
+	//write the JPA interface like ExchangeValueRepo extends JpaRepository<ExchangeValue, Long>
+	//write a method signature in this interface and JPA will provide implementation for the same it 
+	//Spring boot JPA magic
+	
 
 }
